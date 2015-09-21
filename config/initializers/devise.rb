bd959954e73f9b6b3b6c9498d703f9cc8ec994da
Tommy Ngo <ngo.tommy@outlook.com>
2015-09-21 17:49:32 +1200

    remove secret key out of devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,8 +22,7 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
-  
-  config.secret_key = ENV[ 'SECRET_KEY_BASE' ]
+
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
