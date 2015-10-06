3378ebb50d17a5c537b297dfc71b47bdc836d1bd
Mark Aziz <mazi910@aucklanduni.ac.nz>
2015-10-06 16:29:38 +1300

    added initial QR code scanner

--- Android_App/PatientHub/app/src/main/java/patienthub/binary/com/patienthub/MainActivity.java
+++ Android_App/PatientHub/app/src/main/java/patienthub/binary/com/patienthub/MainActivity.java
@@ -1,5 +1,6 @@
 package patienthub.binary.com.patienthub;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this,QR_Code.class);
+        startActivity(i);
     }
 
 
