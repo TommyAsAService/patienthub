6a0a904ed975713011d982e5b2a7f9e57178785d
Tommy Ngo <ngo.tommy@outlook.com>
2015-10-09 12:10:20 +1300

    add feedback table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008020328) do
+ActiveRecord::Schema.define(version: 20151008230918) do
 
   create_table "dosages", force: :cascade do |t|
     t.integer  "patient_id"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20151008020328) do
 
   add_index "dosages", ["patient_id"], name: "index_dosages_on_patient_id"
   add_index "dosages", ["treatment_name"], name: "index_dosages_on_treatment_name"
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.integer  "dosage_id"
+    t.boolean  "taken"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "feedbacks", ["dosage_id"], name: "index_feedbacks_on_dosage_id"
 
   create_table "patients", force: :cascade do |t|
     t.string   "name"
