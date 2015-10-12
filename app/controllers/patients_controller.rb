f7c122b8076cd65cb4e527f80b9484350ece7ff9
Tommy Ngo <ngo.tommy@outlook.com>
2015-10-12 16:06:57 +1300

    add head no content to mailer

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,6 @@
 class PatientsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_patient, only:[:show, :edit, :update, :generate_qr, :mail_to_doctor]
+  before_action :set_patient, only:[:show, :edit, :update, :generate_qr]
 
   autocomplete :treatment, :name
 
@@ -51,8 +51,10 @@ class PatientsController < ApplicationController
   end
 
   def mail_to_doctor
+    set_patient
     DoctorMailer.summary_email(current_user, @patient).deliver
-    render :text=> "Successfully sent email. Please check your mail box.", :status => 200, :content_type => 'text/html'
+    head :no_content
+    # render :text=> "Successfully sent email. Please check your mail box.", :status => 200, :content_type => 'text/html'
   end
 
   private
